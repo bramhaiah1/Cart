d06a3d9885f7f517c9ca960fcbeb8a091b5d1271
naveen s <naveensam.108@gmail.com>
2021-02-04 11:00:07 +0530

    Second commit

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  TouchableHighlight,
+} from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation'
+import { Provider } from 'react-redux';
+import store from './src/store';
+import ShoppingCartIcon from './src/screens/ShoppingCartIcon'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+ import { MaterialIcons } from '@expo/vector-icons';
+
+
+import Home from './src/screens/Home';
+import Cart from './src/screens/ShopingCart';
+import { View ,Text} from 'react-native';
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Home,
   },
+  Cart: {
+    screen: Cart,
+  },
+}, {
+  initialRouteName: 'Home',
+  defaultNavigationOptions : ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: '#2962FF',
+    },
+    headerTintColor: '#fff',
+    headerRight: (props) => (
+    <ShoppingCartIcon/>
+    ),
+  }),
+  headerMode: 'screen',
 });
+const AppContainer = createAppContainer(AppNavigator);
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <AppContainer />
+      </Provider>
+    );
+  }
+}
